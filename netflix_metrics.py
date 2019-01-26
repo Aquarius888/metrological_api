f6963d6424161357f83566ce0dfa8c2df87fda10
Pavel_Alekseev <Pavel_Alekseev@epam.com>
2019-01-26 16:57:15 +0300

    token bullshit

--- netflix_metrics.py
+++ netflix_metrics.py
@@ -4,7 +4,7 @@ import requests
 class Metro(object):
 
     def __init__(self, url_api='', api_type='', metric='', country_id='', timespan='',
-                 timespan_options='', template_type='', app_id='', token='', authorization=''):
+                 timespan_options='', template_type='', app_id='', token=''):
         """
 
         :param url_api: from settings.py
@@ -26,7 +26,6 @@ class Metro(object):
         self.timespan = timespan
         self.timespan_options = timespan_options
         self.template_type = template_type
-        self.authorization = authorization
 
     def widget_call_api(self):
         """Make a request, widget metric.
@@ -37,8 +36,7 @@ class Metro(object):
             '{0}/{1}/{2}?clientCountryId={3}&timespan={4}&timespanOptions={5}&templateType={6}&appId={7}'.\
                 format(self.url_api, self.api_type, self.metric, self.country_id, self.timespan, self.timespan_options,
                        self.template_type, self.app_id)
-        response = requests.get(complete_url, headers={'X-Api-Token': self.token,
-                                                       'Authorization': 'Bearer ' + self.authorization})
+        response = requests.get(complete_url, headers={'X-Api-Token': self.token})
         return response.json()
 
     def application_call_api(self):
@@ -49,8 +47,7 @@ class Metro(object):
         complete_url = \
             '{0}/{1}/{2}?operator=liberty&country={3}&environment=wpe-production&timespan={4}'.\
                 format(self.url_api, self.api_type, self.metric, self.country_id, self.timespan)
-        response = requests.get(complete_url, headers={'X-Api-Token': self.token,
-                                                       'Authorization': 'Bearer ' + self.authorization})
+        response = requests.get(complete_url, headers={'X-Api-Token': self.token})
         return response.json()
 
     def get_allowed_apps_id(self):
@@ -59,8 +56,7 @@ class Metro(object):
         :return: response in json
         """
         complete_url = '{0}/{1}/{2}'.format(self.url_api, self.api_type, self.metric)
-        response = requests.get(complete_url, headers={'X-Api-Token': self.token,
-                                                       'Authorization': 'Bearer ' + self.authorization})
+        response = requests.get(complete_url, headers={'X-Api-Token': self.token})
 
         return response.json()
 
